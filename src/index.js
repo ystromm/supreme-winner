b91361640eb7a39d4b273a53dbc16d347c9b8b72
Martin Carlsson <martin.carlsson@omegapoint.se>
2019-02-17 09:32:32 +0100

    Added API key as parameter.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+if (process.argv.length != 3) {
+    console.log("Need an API key!");
+    process.exit(-1);
+}
+
+const key = process.argv[0];
+
 app.get('/tweets', async (req, res, next) => {
     try {
         res.json({});
